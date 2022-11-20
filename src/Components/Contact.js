6688a591f4b09fa64da58a4c7b1356d7260c0c5b
braincee <kwesi938@gmail.com>
2022-11-20 03:52:11 +0000

    changing state

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 
 class Contact extends Component {
 
-  state = {};
-
-  onShowClick =  e => {};
+  state = {
+    showContactInfo: false
+  };
 
   render() {
+
+  const { showContactInfo } = this.state;
 
   const { name, email, phone} = this.props.contact;
 
   return (
     <div className='card card-body mb-3'>
-     <h6>{name} <i  onClick={this.onShowClick}
+     <h6>{name} <i  onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
         className="fas fa-sort-down" 
      />
      </h6>
-     <ul className='list-group'>
+     {showContactInfo ? ( <ul className='list-group'>
         <li className='list-group-item'> Email: {email}</li>
         <li className='list-group-item'> Phone: {phone}</li>
-     </ul>
+     </ul>) : null}
+    
     </div>
   )
 }
