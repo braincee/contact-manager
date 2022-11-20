8f172c9c57ebfac0b16a3862abafd5669d0d1691
braincee <kwesi938@gmail.com>
2022-11-20 00:32:20 +0000

    Add basics

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Contact from './Components/Contact';
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h2>App Component</h2>
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+     <h4>
+        Stephen
+     </h4>
+     <ul>
+        <li> Email: steve@email.com </li>
+        <li> Phone: 0543431263 </li>
+     </ul>
+    </div>
+  )
+}
+
+export default Contact;
