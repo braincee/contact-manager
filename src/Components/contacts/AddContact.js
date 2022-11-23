3a73f23a7ac35b1adaed9959ef8a5b38e09c6013
braincee <kwesi938@gmail.com>
2022-11-23 02:08:32 +0000

    creating a form with state

---
+++ src/Components/contacts/AddContact.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class AddContact extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        phone: ''
+    };
+
+  render() {
+        const { name, email, phone } = this.state;
+        return (
+         <div className='card mb-3'>
+          <div className='card-header'>Add Contact</div>
+          <div className='card-body'>
+            <form>
+             <div className='form-group'>
+                 <label htmlFor='name'>Name</label>
+                 <input 
+                    type="text"
+                    name="name"
+                    className="form-control form-control-lg"
+                    placeholder='Enter your name ...'
+                    value = {name}
+                 />
+                 
+             </div>
+             <div className='form-group'>
+                 <label htmlFor='name'>Email</label>
+                 <input 
+                    type="email"
+                    name="email"
+                    className="form-control form-control-lg"
+                    placeholder='Enter your email ...'
+                    value = {email}
+                 />
+                 
+             </div>
+             <div className='form-group'>
+                 <label htmlFor='name'>Phone</label>
+                 <input 
+                    type="text"
+                    name="phone"
+                    className="form-control form-control-lg"
+                    placeholder='Enter your phone number ...'
+                    value = {phone}
+                 />
+                 
+             </div>
+             <input type="submit" value="Add Contact"
+             className="btn btn-secondary btn-block mt-2"
+             />
+            </form>
+          </div>
+         </div>
+        );
+    }
+}
+
+
+export default AddContact;
