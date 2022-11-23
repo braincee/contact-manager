5f79cec8047c5965c0533f98f72b4e6637a34d5c
braincee <kwesi938@gmail.com>
2022-11-23 01:27:29 +0000

    Add reducer to context API

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Consumer } from '../context';
 
 class Contact extends Component {
 
@@ -6,33 +7,41 @@ class Contact extends Component {
     showContactInfo: false
   };
 
-  onDeleteClick = () => {
-    this.props.deleteClickHandler();
+  onDeleteClick = (id, dispatch) => {
+    dispatch({type: 'DELETE_CONTACT', payload: id})
   }
 
   render() {
 
   const { showContactInfo } = this.state;
 
-  const { name, email, phone} = this.props.contact;
+  const { id, name, email, phone} = this.props.contact;
 
   return (
-    <div className='card card-body mb-3'>
-     <h6>{name} <i  onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
-        className="fas fa-sort-down" 
-        style={{cursor: 'pointer'}}
-     />
-     <i className='fas fa-times' 
-     style={{cursor: 'pointer', float: 'right', color: 'red'}}
-     onClick={this.onDeleteClick}
-     />
-     </h6>
-     {showContactInfo ? ( <ul className='list-group'>
-        <li className='list-group-item'> Email: {email}</li>
-        <li className='list-group-item'> Phone: {phone}</li>
-     </ul>) : null}
-    
-    </div>
+
+  <Consumer>
+    {value => {
+      const { dispatch } = value;
+      return (
+        <div className='card card-body mb-3'>
+        <h6>{name} <i  onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
+           className="fas fa-sort-down" 
+           style={{cursor: 'pointer'}}
+        />
+        <i className='fas fa-times' 
+        style={{cursor: 'pointer', float: 'right', color: 'red'}}
+        onClick={this.onDeleteClick.bind(this, id, dispatch)}
+        />
+        </h6>
+        {showContactInfo ? ( <ul className='list-group'>
+           <li className='list-group-item'> Email: {email}</li>
+           <li className='list-group-item'> Phone: {phone}</li>
+        </ul>) : null}
+       
+       </div>
+      )
+    }}
+  </Consumer>
   )
 }
 }
